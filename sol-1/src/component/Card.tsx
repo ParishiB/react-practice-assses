48e872d57899ffd0c3d3c59fd6fef5a936ab6bc3
PARISHI BHANGE <parishibhange@PARISHIs-MacBook-Pro.local>
2024-05-15 16:54:17 +0530

    spinner added

--- sol-1/src/component/Card.tsx
+++ sol-1/src/component/Card.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { RingSpinner } from "react-spinners-kit";
 
 interface User {
   id: number;
@@ -34,27 +35,58 @@ const Card: React.FC<CardProps> = ({ name }) => {
 
   return (
     <div className="card">
-      {!b && (
-        <React.Fragment>
-          <div>{name}</div>
-          <button onClick={fetchDetails}>Get Users</button>
-        </React.Fragment>
-      )}
-      {b && users.length > 0 && (
-        <div>
-          <h2>User Details:</h2>
-          <ul>
-            {users.map((user) => (
-              <li key={user.id}>
-                <strong>Name:</strong> {user.first_name} {user.last_name}
-                <br />
-                <strong>Email:</strong> {user.email}
-                <br />
-                <img src={user.avatar} alt={`Avatar of ${user.first_name}`} />
-              </li>
-            ))}
-          </ul>
+      {loading ? (
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <RingSpinner />
         </div>
+      ) : (
+        <>
+          {!b && (
+            <div
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <div>{name}</div>
+              <button onClick={fetchDetails}>Get Users</button>
+            </div>
+          )}
+          {b && users.length > 0 && (
+            <div>
+              <h2>User Details:</h2>
+              <ul>
+                {users.map((user) => (
+                  <li key={user.id}>
+                    <strong>Name:</strong> {user.first_name} {user.last_name}
+                    <br />
+                    <strong>Email:</strong> {user.email}
+                    <br />
+                    <img
+                      src={user.avatar}
+                      alt={`Avatar of ${user.first_name}`}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
